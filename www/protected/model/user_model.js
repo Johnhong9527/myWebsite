619eb4cbefca402fcbadb50cfc84ff76b39d5f6d
seam <honghaitzz11@gmail.com>
2018-03-13 22:31:32 +0800

    .

---
+++ www/protected/model/user_model.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+	name:  String,
+	passwd:String,
+	address:String,
+	email: String,
+	phone: Number,
+	regTime:Number
+});
+
+module.exports = mongoose.model('users', userSchema); 
--- www/protected/routes/index.js
+++ www/protected/routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var userInfo = require('../util/userInfo')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	res.render('index', { title: 'Express' });
 });
+
+router.get('/api',function(req,res,next){
+	const that = res;
+	userInfo({
+		name:'李四',
+		passwd:'dsdd2*^1dsd&^^3fsd',
+		address:'浦电路',
+		email:'1310512423@qq.com',
+		phone:15868600329
+	},res=>that.send(res));
+})
 
 module.exports = router;
