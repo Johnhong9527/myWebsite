dc96037faff41f99b29ab77e97e375bbd0e04dd5
seam <honghaitzz11@gmail.com>
2018-03-21 13:28:33 +0800

    新增若干内容

--- www/protected/util/mongoed.js
+++ www/protected/util/mongoed.js
@@ -1,17 +1,40 @@
 const mongoose = require('mongoose');
-const users = require('../model/user_model')
+mongoose.Promise = global.Promise;
+console.log(3);
+const GITHUB_ISSUE = `test`;
+const connectionString = `mongodb://jsmith:some-initial-password@localhost:27017/${ GITHUB_ISSUE }`;
+
+function userInfo(){
+	console.log(7);
+	run().catch(error=> console.error(error.stack));
+	console.log(9);
+	async function run() {
+		await mongoose.connect(connectionString);
+
+		const schema = new mongoose.Schema({ name: String })
+
+		const Model = mongoose.model('Model', schema);
+
+		console.log(await Model.create({ name: 'test' }));
+
+
+	}
+}
+
+
+/*const users = require('../model/user_model')
 function userInfo(params,cb){
-	mongoose.connect('mongodb://localhost:27017/test');
+	mongoose.connect('mongodb://127.0.0.1:27017/test');
 
 	params.regTime = Date.parse(new Date())/1000
 	const user = new users(params);
-	user.save().then(() => cb('ok'));
+	user.save().then(() => cb('ok'));*/
 
 	/*const Cat = mongoose.model('Cat', { name: String });
 	const kitty = new Cat({ name: 'Zildjian' });
 	kitty.save().then(() => cb('ok'));*/
-}
-module.exports = userInfo;
+	/*}*/
+	module.exports = userInfo;
 /*
 usernfo 
 	set
