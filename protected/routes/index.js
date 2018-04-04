c9f75067b0761d32e6bf0fd9699bd5866d534794
seam <honghaitzz11@gmail.com>
2018-04-04 15:23:11 +0800

    .

--- protected/routes/index.js
+++ protected/routes/index.js
@@ -54,7 +54,7 @@ router.all('/cros', function(req, res, next) {
     _url = req.url,
     _index = _url.indexOf('http');
   _url = req.url.slice(_index);
-  console.log(req.url);
+  console.log(req.body.url);
   console.log(_url);
   request
     .post(_url)
