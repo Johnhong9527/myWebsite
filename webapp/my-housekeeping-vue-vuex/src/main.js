363af8f544ff88a3428b897ace1238de7c28984f
seam <honghaitzz11@gmail.com>
2018-04-13 13:02:32 +0800

    .

--- webapp/my-housekeeping-vue-vuex/src/main.js
+++ webapp/my-housekeeping-vue-vuex/src/main.js
@@ -4,7 +4,10 @@ import Vue from "vue";
 import App from "./App";
 import router from "./router";
 import store from "./store";
+import XHeader from "./components/XHeader";
 
+// 全局组件
+Vue.component("x-header", XHeader);
 Vue.config.productionTip = false;
 /* eslint-disable no-new */
 new Vue({
