0205b9fcbc1350bf62501623f1d693c53ce561ae
seam <honghaitzz11@gmail.com>
2018-04-16 15:03:03 +0800

    爬虫

---
+++ webapp/node_vue_read/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style lang="less">
+@import url('./assets/style.less');
+* {
+  padding: 0;
+  margin: 0;
+}
+</style>
