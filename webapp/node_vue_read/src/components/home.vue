7356588acd1debdb73bea4c7f7672e87e897ace5
seam <honghaitzz11@gmail.com>
2018-04-16 15:03:03 +0800

    爬虫

---
+++ webapp/node_vue_read/src/components/home.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <div>
+      <div>
+        <img :src="info.avatar" alt="">
+      </div>
+      <aVue/>
+      <!-- <bVue/> -->
+    </div>
+  </div>
+</template>
+
+<script>
+// import { store, mapState, mapMutations, mapActions } from 'vuex'
+import aVue from './a';
+import bVue from './b';
+// import config from '@/config/config'
+let config = require('@/config/config');
+export default {
+  name: 'home',
+  data() {
+    return {
+      info: {},
+    };
+  },
+  created() {
+    console.log(config.apiPath());
+    this.$store.commit('GET_USERINFO');
+  },
+  methods: {},
+  components: {
+    aVue,
+    bVue,
+  },
+};
+</script>
+
+<style scoped lang='less'>
+button {
+  padding: 15px;
+  cursor: pointer;
+  user-select: none;
+}
+img {
+  display: block;
+  width: 100%;
+  border-radius: 50%;
+  &::before {
+    content: '';
+    display: block;
+  }
+}
+</style>
---
+++ webapp/node_vue_read/src/store/modules/user.js
@@ -0,0 +1,58 @@
+import { getUser, getUserRole } from "@/config/api";
+import config from "@/config/config";
+
+// 常量
+const GET_USERINFO = "GET_USERINFO"; // 用户信息
+const GET_USERROLE = "GET_USERROLE"; // 用户信息
+
+// state
+const state = {
+  user_id: localStorage.getItem("wxUserID1") != null ? localStorage.getItem("wxUserID1") : "",
+  info: {},
+  is_teacher: null
+};
+
+// getters
+const getters = {};
+
+// actions
+const actions = {};
+
+// mutations
+const mutations = {
+  GET_USERINFO(state) {
+    getUser(state.user_id).then(res => {
+      state.info = res.data.data;
+    });
+  },
+  GET_USERROLE(state, self) {
+    setTimeout(() => {
+      getUserRole(state.user_id).then(res => {
+        state.is_teacher = res.data.is_teacher;
+        if (state.is_teacher === 1) {
+          self.$router.push({ path: "/h/info" });
+        } else if (state.is_teacher === 2) {
+          self.$router.push({ path: "/m/list" });
+        } else {
+          if (
+            self.$router.currentRoute.path !== "/order" &&
+            self.$router.currentRoute.path !== "/order/list" &&
+            self.$router.currentRoute.path !== "/order/add"
+          ) {
+            self.$router.push({ path: "/" });
+          } else {
+            self.$router.push({ path: "/order/add" });
+          }
+        }
+        self = null;
+      });
+    }, 100);
+  }
+};
+
+export default {
+  state,
+  getters,
+  actions,
+  mutations
+};
