c25129dbc6fc1510107bea7d629638c775efc5e9
洪海涛 <honghaitzz11@gmail.com>
2018-04-23 16:25:53 +0800

    .

--- webapp/node_vue_read/src/config/config.js
+++ webapp/node_vue_read/src/config/config.js
@@ -1,7 +1,8 @@
 module.exports = {
   apiPath() {
     if (process.env.NODE_ENV == 'development') {
-      return 'https://api.honghaitao.net/'
+      // return 'https://api.honghaitao.net/'
+      return 'http://127.0.0.1:3000/'
     } else {
       return ''
     }
