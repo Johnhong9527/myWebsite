040ab214db45b6b271b3c743d1ade39c5bd684d2
seam <honghaitzz11@gmail.com>
2018-05-03 10:05:51 +0800

    修改请求数据源

--- webapp/node_vue_read/build/push.sh
+++ webapp/node_vue_read/build/push.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+## 站点根目录，存在多个项目，需要将项目中的路径指向到对应目标
 sed -i 's/\"static\/js\/\"/\"kindle\/static\/js\/\"/g' ./dist/static/js/manifest.*.js
+sed -i 's/\/static\//\/kindle\/static\//g' ./dist/index.html
 rm -rf ../../protected/public/kindle/*
 cp -r dist/* ../../protected/public/kindle/
