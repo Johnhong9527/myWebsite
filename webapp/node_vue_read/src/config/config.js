a109628ead583f15bbfdabdad38d0d0b545fc6c6
seam <honghaitzz11@gmail.com>
2018-05-03 09:39:23 +0800

    修改请求数据源

--- webapp/node_vue_read/src/config/config.js
+++ webapp/node_vue_read/src/config/config.js
@@ -4,7 +4,8 @@ module.exports = {
       return 'https://api.honghaitao.net/'
       // return 'http://127.0.0.1:3000/'
     } else {
-      return 'http://127.0.0.1:3000/'
+      // return 'http://127.0.0.1:3000/'
+      return ''
     }
   }
 };
